adcd334942fbc8a457dec4975c5457b43ec9a0b4
Bao Long Kong <baolong.kong@htw-dresden.de>
2020-11-10 20:11:18 +0100

    testing for 2 tactosy...

--- src/settings.js
+++ src/settings.js
@@ -1,4 +1,4 @@
-// import tactJs from 'tact-js';
+import tactJs from 'tact-js';
 
 const btn_test = document.getElementById("test");
 const btn_multitest = document.getElementById("multitest");
@@ -14,14 +14,6 @@ var points = [
   {
     index: 0,
     intensity: 50
-  },
-  {
-    index: 1,
-    intensity: 50
-  },
-  {
-    index: 2,
-    intensity: 50
   }
 ];
 var durationMillis = 200;
@@ -34,28 +26,29 @@ var instrument1Outs;
 var instrument2Ins;
 var instrument2Outs;
 
-// tactJs.addListener(function(msg) {
-//     if (msg.status === 'Connected') {
-//       // console.log('connected');
-//     } else if (msg.status === 'Disconnected') {
+tactJs.addListener(function(msg) {
+    if (msg.status === 'Connected') {
+      // console.log('connected');
+    } else if (msg.status === 'Disconnected') {
   
-//     } else if (msg.status === 'Connecting') {
-//       // 
-//     }
-// });
+    } else if (msg.status === 'Connecting') {
+      // 
+    }
+});
 
 btn_test.addEventListener("click", test);
 btn_multitest.addEventListener("click", multitest);
 
-//errorcodes: 0 - success, 2 - connection not established
-// function test() {
-//   console.log(tactJs.submitDot(key, position, points, durationMillis));
-// }
+// errorcodes: 0 - success, 2 - connection not established
+function test() {
+  console.log(tactJs.submitDot(key, positionL, points, durationMillis));
+  console.log(tactJs.submitDot(key, positionR, points, durationMillis));
+}
 
-// function multitest() {
-//   var interval = setInterval(() => {console.log(tactJs.submitDot(key, position, points, durationMillis))}, 1000);
-//   setTimeout(() => {clearInterval(interval)}, 4000);
-// }
+function multitest() {
+  var interval = setInterval(() => {console.log(tactJs.submitDot(key, position, points, durationMillis))}, 1000);
+  setTimeout(() => {clearInterval(interval)}, 4000);
+}
 
 inp_songtest.addEventListener("change", function() {
   var reader = new FileReader();
@@ -106,19 +99,23 @@ function audiotest({
 btn_songtest.addEventListener("click", songtest);
 
 function songtest() {
-  // for (let i = 0; i < instrument1Ins.length; i++) {
-  //   setTimeout(() => {countin_out(positionL)}, instrument1Ins[i])
-  // }
-  // for (let i = 0; i < instrument1Outs.length; i++) {
-  //   setTimeout(() => {countin_out(positionL)}, instrument1Outs[i])
-  // }
+  test2();
+  for (let i = 0; i < instrument1Ins.length; i++) {
+    setTimeout(() => {countin_out(positionL)}, instrument1Ins[i])
+  }
+  for (let i = 0; i < instrument1Outs.length; i++) {
+    setTimeout(() => {countin_out(positionL)}, instrument1Outs[i])
+  }
+  setTimeout(() => {startsong()}, (bps * beat[0]));
+}
+
+function test2() {
   for (let i = 0; i < instrument2Ins.length; i++) {
     setTimeout(() => {countin_out(positionR)}, instrument2Ins[i])
   }
   for (let i = 0; i < instrument2Outs.length; i++) {
     setTimeout(() => {countin_out(positionR)}, instrument2Outs[i])
   }
-  setTimeout(() => {startsong()}, (bps * beat[0]));
 }
 
 function startsong() {
@@ -139,7 +136,7 @@ function calculatecountms(in_out) {
 }
 
 function countin_out(position) {
-  // var interval = setInterval(() => {console.log(tactJs.submitDot(key, position, points, durationMillis))}, (bps * 1000));
-  var interval = setInterval(() => {console.log("Beat " + position)}, (bps * 1000));
+  var interval = setInterval(() => {console.log(tactJs.submitDot(key, position, points, durationMillis))}, (bps * 1000));
+  // var interval = setInterval(() => {console.log("Beat " + position)}, (bps * 1000));
   setTimeout(() => {clearInterval(interval)}, ((bps * 1000) * beat[0]));
 }
